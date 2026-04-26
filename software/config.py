5823cc9eaf424a4331a184ce8d59fda42fe8adf2
Nihita <151723513+Nihita21@users.noreply.github.com>
2026-04-26 13:17:11 +0530

    Update config.py

--- software/config.py
+++ software/config.py
@@ -2,8 +2,8 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'scgwms-secret-key-2026')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://nihitagupta:Nihita@cluster0.dv6wreg.mongodb.net/scgwms_db?retryWrites=true&w=majority')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_URI = os.environ.get('MONGO_URI')
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5 MB max upload
     UPLOAD_FOLDER = 'static/uploads'
